c3afcbba9e224433b82991c464b587c42810304d
AlexKarmaz <karmazct@mail.ru>
2018-05-25 13:20:59 +0300

    add task1

--- ControllerAnalyzer/ControllerAnalyzer/CodeFixProvider.cs
+++ ControllerAnalyzer/ControllerAnalyzer/CodeFixProvider.cs
@@ -18,7 +18,8 @@ namespace ControllerAnalyzer
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ControllerAnalyzerCodeFixProvider)), Shared]
     public class ControllerAnalyzerCodeFixProvider : CodeFixProvider
     {
-        private const string title = "Make uppercase";
+        private const string Title = "Add 'Controller' suffix";
+        private const string ControllerSuffix = "Controller";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -27,7 +28,6 @@ namespace ControllerAnalyzer
 
         public sealed override FixAllProvider GetFixAllProvider()
         {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
             return WellKnownFixAllProviders.BatchFixer;
         }
 
@@ -35,38 +35,32 @@ namespace ControllerAnalyzer
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
             var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
 
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
                 CodeAction.Create(
-                    title: title,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
-                    equivalenceKey: title),
+                    title: Title,
+                    createChangedSolution: c => AddControllerSuffixAsync(context.Document, declaration, c),
+                    equivalenceKey: Title),
                 diagnostic);
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Solution> AddControllerSuffixAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
             var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            var newName = identifierToken.Text + ControllerSuffix;
 
-            // Get the symbol representing the type to be renamed.
             var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
             var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
 
-            // Produce a new solution that has all references to that type renamed, including the declaration.
             var originalSolution = document.Project.Solution;
             var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+            var newSolution = await Renamer.RenameSymbolAsync(originalSolution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
 
-            // Return the new solution with the now-uppercase type name.
             return newSolution;
         }
     }
--- ControllerAnalyzer/ControllerAnalyzer/DiagnosticAnalyzer.cs
+++ ControllerAnalyzer/ControllerAnalyzer/DiagnosticAnalyzer.cs
@@ -20,30 +20,43 @@ namespace ControllerAnalyzer
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
+        private const string ControllerSuffix = "Controller";
+
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
             var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var mvcController = context.Compilation.GetTypeByMetadataName("System.Web.Mvc.Controller");
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            var baseClasses = GetBaseClasses(namedTypeSymbol);
+            if (baseClasses.Contains(mvcController) && !namedTypeSymbol.Name.EndsWith(ControllerSuffix))
             {
-                // For all such symbols, produce a diagnostic.
                 var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
 
                 context.ReportDiagnostic(diagnostic);
             }
+        }
+
+        private static List<INamedTypeSymbol> GetBaseClasses(INamedTypeSymbol namedTypeSymbol)
+        {
+            var baseClasses = new List<INamedTypeSymbol>();
+
+            var currentTypeSymbol = namedTypeSymbol;
+            while (currentTypeSymbol.BaseType != null && currentTypeSymbol.BaseType.TypeKind != TypeKind.Error)
+            {
+                baseClasses.Add(currentTypeSymbol.BaseType);
+                currentTypeSymbol = currentTypeSymbol.BaseType;
+            }
+
+            return baseClasses;
         }
     }
 }
